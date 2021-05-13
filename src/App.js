23fca6ad721267ea49028094cb974cdc7b58eaf0
Venus <tmdiasdutra@icloud.com>
2021-05-13 16:05:47 -0300

    Estilizacao do Site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import img from './Imagens/Background.png';
+import img2 from './Imagens/Background2.png'
 // import Produtos from './Components/Produtos/Produtos'
 // import ItemCarrinho from './Components/ItemCarrinho/ItemCarrinho'
 
@@ -9,6 +11,7 @@ const MainContainer = styled.div`
  gap: 10px;
  justify-content: space-between;
  width: 100%;
+ background-color: lightgrey;
 `
 
 const ContainerFiltros = styled.div`
@@ -16,6 +19,11 @@ display: flex;
 flex-direction: column;
 border: 1px solid black;
 width: 20%;
+height: 100vh;
+background-image: url(${img});
+height: 100vh;
+color: white;
+padding: 10px
 `
 
 const ContainerCentral = styled.div`
@@ -29,6 +37,9 @@ display: flex;
 flex-direction: column;
 border: 1px solid black;
 width: 20%;
+background-image: url(${img2});
+color: white;
+padding: 10px
 `
 const ContainerIndiceCentral = styled.div`
 display: flex;
@@ -39,10 +50,15 @@ const ContainerProdutos = styled.div`
 display: flex;
 flex-wrap:wrap;
 gap: 5px;
+justify-content: space-between;
 `
 const Produtos = styled.div`
 display: flex;
 flex-direction: column;
+border: 1px solid black;
+flex-wrap: wrap;
+align-items: center;
+
 /* width: 25%; */
 
 img {
@@ -58,38 +74,62 @@ class App extends React.Component {
     listaProdutos: [
       {
         id: 1,
-        name: "Viagem para lua",
-        value: 10000.0,
-        imageUrl: "https://bioscopic.files.wordpress.com/2008/01/moon.jpg",
+        name: "Viagem colônia Lunar",
+        value: 250000.0,
+        imageUrl: "https://i.pinimg.com/564x/08/72/dc/0872dc1108d6bd8a82f56a52c20fffaf.jpg",
       },
       {
         id: 2,
-        name: "Viagem para marte",
-        value: 30000.0,
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/800px-OSIRIS_Mars_true_color.jpg",
+        name: "Viagem colônia em Marte",
+        value: 375000.0,
+        imageUrl: "https://i.pinimg.com/564x/6c/ac/bb/6cacbb5e5aad0a31f58272881c4b0e8a.jpg",
       },
       {
         id: 3,
-        name: "Viagem para venus",
-        value: 35000.0,
-        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/8/85/Venus_globe.jpg",
+        name: "Viagem colônia flutuante Vênus",
+        value: 450000.0,
+        imageUrl: "https://i.pinimg.com/564x/d4/50/59/d45059ce55c038dee6af059f38b10fcf.jpg",
       },
       {
         id: 4,
-        name: "Viagem só de ida para sol",
-        value: 50000.0,
-        imageUrl: "https://stcotvfoco.com.br/2017/03/mira-el-antes-y-despues-del-sol-de-los-teletubbies-01.jpg",
+        name: "Viagem de ida para o Sol",
+        value: 650500.0,
+        imageUrl: "https://i.pinimg.com/564x/53/97/4c/53974cc2fcf2d0008f293a1fefdf026c.jpg",
       },
       {
         id: 5,
-        name: "Tour em buraco negro",
-        value: 900000.0,
-        imageUrl: "https://ibcdn.canaltech.com.br/yuxagEBwKbCP_kyKwD17behqE-o=/660x0/smart/i360616.jpeg",
+        name: "Expedição para Buraco Negro",
+        value: 870000.0,
+        imageUrl: "https://i.pinimg.com/564x/8a/17/78/8a1778090c871a078d0add0498058d76.jpg",
       },
       {
         id: 6,
-        name: "Pacote Premium - aos limites do universo",
-        value: 9999999.0,
+        name: "Viagem para colônia Espacial",
+        value: 300000.0,
+        imageUrl: "https://i.pinimg.com/564x/1a/6b/f9/1a6bf99078f9f472a57629ac28f72fa9.jpg",
+      },
+      {
+        id: 7,
+        name: "Férias no Hotel Intergaláctico",
+        value: 475000.0,
+        imageUrl: "https://i.pinimg.com/564x/8e/a3/18/8ea318461bdb245c07500b29d6f3b7e5.jpg",
+      },
+      {
+        id: 8,
+        name: "Férias na Colônia Espacial",
+        value: 660000.0,
+        imageUrl: "https://i.pinimg.com/564x/7f/e2/94/7fe29480e5fd7991413b00d0d94bc54d.jpg",
+      },
+      {
+        id: 9,
+        name: "Tour pelo Sistema Solar",
+        value: 900500.0,
+        imageUrl: "https://i.pinimg.com/564x/db/80/58/db8058b2a7a337933c8819741d1f3961.jpg",
+      },
+      {
+        id: 10,
+        name: "Pacote - aos Limites do Universo",
+        value: 999999.0,
         imageUrl: "http://www.reactiongifs.com/wp-content/uploads/2013/10/tim-and-eric-mind-blown.gif",
       }
     ],
@@ -191,7 +231,7 @@ class App extends React.Component {
 
         <ContainerCentral>
           <ContainerIndiceCentral>
-            <p>Quantidade de produtos: {this.state.listaProdutos.length} </p>
+            <h4>Quantidade de produtos: {this.state.listaProdutos.length} </h4>
 
             <label>Ordenação:
               <select onChange={this.ordenaLista} >
